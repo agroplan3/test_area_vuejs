7ae81b9972bcb66df5b0f1913d7bc37738987d5f
Zerescas <zerescas@gmail.com>
2019-03-11 18:56:43 +0900

    Switch to Uikit

--- src/app.vue
+++ src/app.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app" class="uk-container uk-container-center uk-text-center uk-margin-large-top">
-    <h1>{{ msg }}</h1>
+    <h1 class="uk-animation-slide-top">{{ msg }}</h1>
+
     <ul class="uk-subnav uk-flex-center">
-      
-      <li>
-        <a href="https://google.com" target="_blank">
-          <img style="width:100px; height:100px" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg">
-          <p style="margin:0px">Go to Google.com</p>
-        </a>
-      </li>
-      
-      <li>
-        <a href="https://github.com" target="_blank">
-          <img style="width:100px; height:100px" src="https://image.flaticon.com/icons/png/512/25/25231.png">
-          <p style="margin:0px">Go to Github.com</p>
+  
+      <button style="padding:0px" class="uk-button uk-button-default uk-animation-slide-top">
+          <a href="https://google.com" target="_blank">
+            <img style="width:100px; height:100px" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg">
           </a>
-      </li>
+          <span class="uk-label">Go To Google.com</span>
+      </button>
+
+      <button style="padding:0px" class="uk-button uk-button-default uk-animation-slide-top">
+          <a href="https://github.com" target="_blank">
+            <img style="width:100px; height:100px" src="https://image.flaticon.com/icons/png/512/25/25231.png">
+          </a>
+          <span class="uk-label">Go To Github.com</span>
+      </button>
 
     </ul>
-    <vk-button v-on:click="customFunc">Vuikit Button</vk-button>
+
+  <vk-button v-on:click="customFunc">Vuikit Button</vk-button>
+
   </div>
 </template>
 
@@ -32,7 +35,7 @@ export default {
   },
   data () {
     return {
-      msg: 'Test Vue Vuikit App'
+      msg: 'Test Vue Uikit App'
     }
   }
 }
